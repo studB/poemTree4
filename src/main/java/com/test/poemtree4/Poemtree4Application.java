6332bdd00c51aa652cfcff638aaebb6190594498
bdy <bdy@bdyui-Macmini.local>
2021-03-04 19:54:45 +0900

    init Commit

---
+++ src/main/java/com/test/poemtree4/Poemtree4Application.java
@@ -0,0 +1,13 @@
+package com.test.poemtree4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poemtree4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poemtree4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/poemtree4/Poemtree4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.poemtree4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poemtree4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
