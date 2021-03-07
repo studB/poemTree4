8d17bac63e088f186bb20f70def8e522019151a0
bdy <bdy@bdyui-Macmini.local>
2021-03-07 14:30:27 +0900

    Completed: Business Logic Contruction

--- src/main/java/com/test/poemtree4/poem/PoemController.java
+++ src/main/java/com/test/poemtree4/poem/PoemController.java
@@ -1,14 +1,174 @@
 package com.test.poemtree4.poem;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.experimental.PackagePrivate;
 
 @Controller
 public class PoemController{
 
+    private PoemRepository poemRepository;
+
+    @Autowired
+    public PoemController(PoemRepository poemRepository){
+        this.poemRepository = poemRepository;
+    }
 
     @RequestMapping(value = "/")
     public String mainRendering(){
         return "note";
     }
+
+    // Poem Logic
+
+
+    @RequestMapping(value = "/api/get/allfolder")
+    @ResponseBody
+    public List<FolderObject> getAllFolderList(){
+        return poemRepository.findAllA();
+    }
+
+
+    // Folder open - test ok
+    // Get Contents list in clicked folder
+    @RequestMapping( value = "/api/get/contents/{tableId}")
+    @ResponseBody
+    public List<PoemObject> getAllContentsInFolder(@PathVariable(required = true) int tableId){
+       return poemRepository.findByIdW(tableId);
+    }
+
+
+    // Create Folder - test ok
+    // Make folder table and insert a folder information into admintable
+    @RequestMapping( value = "/api/create/folder/{folderName}")
+    @ResponseBody
+    public int createNewFolder(@PathVariable(required = true) String folderName){
+
+        int tableId = poemRepository.insertA(folderName);
+        poemRepository.createTableF(tableId);
+
+        return tableId;
+    }
+
+    // Rename Folder
+    @RequestMapping( value = "/api/rename/folder/{tableId}/{newName}")
+    @ResponseBody
+    public String renameFolder(@PathVariable(required = true) int tableId, @PathVariable(required = true) String newName){
+        poemRepository.updateNameA(tableId, newName);
+
+        return newName;
+    }
+
+
+    // Click contents - test ok
+    // Get Contents, worked one and backup 
+    @RequestMapping( value = "/api/get/content/{tableId}/{contentId}")
+    @ResponseBody
+    public Map<String, Object> getContent(@PathVariable(required = true) int tableId, @PathVariable(required = true) String contentId){
+        
+        Map<String, Object> returnSet = new HashMap<String, Object>();
+        returnSet.put("work", poemRepository.findContentByContentIdW(contentId));
+        returnSet.put("backup", poemRepository.findByContentIdF(tableId, contentId));
+
+        return returnSet;
+    } 
+
+
+    // create new content - test ok
+    // start to the work
+    @RequestMapping( value = "/api/create/content/{tableId}")
+    @ResponseBody
+    public String createNewContent(@PathVariable(required = true) int tableId){
+
+        String contentId = UUID.randomUUID().toString();
+
+        PoemObject poemObject = new PoemObject();
+        poemObject.setContentId(contentId);
+        
+        poemRepository.insertW(tableId, contentId);
+        
+        return contentId;
+    }
+
+
+    // Save worked Contents - test ok
+    // Update Worked contents
+    @PostMapping( value = "/api/save/content")
+    @ResponseBody
+    public String saveContent(@RequestBody PoemObject poemObject){
+        poemRepository.updateAnyW(poemObject);
+        return "done";
+    }
+
+
+    // Publish Worked Content into backup house - test ok
+    // insert data and get backup contens again
+    @PostMapping( value = "/api/backup/content/{tableId}")
+    @ResponseBody
+    public String backUpContent(@PathVariable(required = true) int tableId, @RequestBody PoemObject poemObject){
+        
+        poemRepository.insertF(tableId, poemObject);
+
+        return "done";
+    }
+
+
+    // Erase backup one - test ok
+    // erase content
+    @RequestMapping( value = "/api/delete/backup/{tableId}/{date}")
+    @ResponseBody
+    public String eraseBackUp(@PathVariable(required = true) int tableId, @PathVariable(required = true) String date){
+
+        poemRepository.eraseF(tableId, date);
+
+        return "done";
+    }
+
+    // Delete contents test ok
+    // erase content all that conttains worked and backup, and erase content info from all table
+    @RequestMapping( value = "/api/delete/content/{tableId}/{contentId}")
+    public String deleteContent(@PathVariable(required = true) int tableId, @PathVariable(required = true) String contentId){
+
+        // delete backup
+        poemRepository.deleteByContentIdF(tableId, contentId);
+
+
+        // delete from workingtable
+        poemRepository.deleteContentW(contentId);
+
+        return "redirect:/";
+    }
+    
+    
+    // Delete Folder
+    // delete folder information and related table, all contents of folder is also deleted. 
+    @RequestMapping( value = "/api/delete/folder/{tableId}")
+    @ResponseBody
+    public String deleteFolder(@PathVariable(required = true) int tableId){
+
+        //delete folder table
+        poemRepository.deleteTableF(tableId);
+
+        //delete from workingtable
+        poemRepository.deleteFolderW(tableId);
+
+        //delete from admintable
+        poemRepository.deleteA(tableId);
+
+        return "done";
+    }
+
+
+
 }
---
+++ src/test/java/com/test/poemtree4/APITest.java
@@ -0,0 +1,183 @@
+package com.test.poemtree4;
+
+import static org.mockito.ArgumentMatchers.refEq;
+
+import javax.swing.text.AbstractDocument.Content;
+
+import com.test.poemtree4.poem.PoemController;
+import com.test.poemtree4.poem.PoemObject;
+import com.test.poemtree4.poem.PoemRepository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+// https://spring.io/guides/gs/testing-web/
+// https://www.baeldung.com/spring-boot-testing
+/*
+@TestPropertySource(
+  locations = "classpath:application-test.properties")
+*/
+
+
+@SpringBootTest(
+    //SpringBootTest.WebEnvironment.MOCK
+)
+@AutoConfigureMockMvc
+public class APITest {
+    
+    @Autowired
+    private PoemController poemController;
+
+    @Autowired
+    private PoemRepository poemRepository;
+
+    @Test
+    void controllerLoad() throws Exception{
+        Assertions.assertThat(poemController).isNotNull();
+        Assertions.assertThat(poemRepository).isNotNull();
+    }
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    MockMvcRequestBuilders mrb;
+    MockMvcResultHandlers mrh;
+    MockMvcResultMatchers mrm;
+
+    @Test
+    void getAllFolderListTest() throws Exception{
+        mockMvc
+            .perform(mrb.get("/api/get/allfolder"))
+            .andDo(mrh.print())
+            .andExpect(mrm.status().isOk());
+    }
+
+    @Test
+    void getAllContentsInFolderTest() throws Exception{
+        mockMvc
+            .perform(mrb.get("/api/get/contents/{tableid}", 6))
+            .andDo(mrh.print())
+            .andExpect(mrm.status().isOk());
+    }
+
+    @Test
+    void renameFolderTest() throws Exception{
+        mockMvc
+            .perform(mrb.get("/api/rename/folder/{tableId}/{newName}", 6, "newName"))
+            .andDo(mrh.print())
+            .andExpect(mrm.status().isOk());
+    }
+
+    @Test
+    void createNewFolderTest() throws Exception{
+        
+        mockMvc
+            .perform(mrb.get("/api/create/folder/{folderName}", "testFolder"))
+            .andDo(mrh.print())
+            .andExpect(mrm.status().isOk())
+            .andReturn();
+
+    }
+
+    @Test
+    void createNewContentTest() throws Exception{
+
+        mockMvc
+            .perform(mrb.get("/api/create/content/{tableId}", 6))
+            .andDo(mrh.print())
+            .andExpect(mrm.status().isOk());
+
+        for(PoemObject p : poemRepository.findByIdW(6)){
+            System.out.println(p.toString());
+        }
+    }
+
+    @Test
+    void getContentTest() throws Exception{
+        mockMvc
+            .perform(mrb.get("/api/get/content/{tableId}/{contentId}",6,"3126ce02-acf8-4b0b-b1c7-efe61e752f9f"))
+            .andDo(mrh.print())
+            .andExpect(mrm.status().isOk());
+    }
+
+
+
+    @Test
+    void saveContentTest() throws Exception{
+        
+        String poemContent ="{\"contentId\" : \"3126ce02-acf8-4b0b-b1c7-efe61e752f9f\", " +
+                            "\"title\" : \"title\", " + 
+                            "\"body\" : \"body\", " + 
+                            "\"date\" : \"date\"}";
+
+        RequestBuilder request = mrb.post("/api/save/content")
+                                        .content(poemContent)
+                                        .contentType("application/json");
+
+        mockMvc
+            .perform(request)
+            .andDo(mrh.print())
+            .andExpect(mrm.status().isOk());
+    }
+
+    @Test
+    void backUpContentTest() throws Exception{
+
+        int tableId = 1;
+        String poemContent ="{\"contentId\" : \"3126ce02-acf8-4b0b-b1c7-efe61e752f9f\", " +
+                            "\"title\" : \"title1\", " + 
+                            "\"body\" : \"body1\", " + 
+                            "\"date\" : \"date1\"}";
+
+        RequestBuilder request = mrb.post("/api/backup/content/{tableId}", 6)
+                                        .content(poemContent)
+                                        .contentType("application/json");
+
+        mockMvc
+            .perform(request)
+            .andDo(mrh.print())
+            .andExpect(mrm.status().isOk());
+
+    }
+
+    @Test
+    void eraseBackUpTest() throws Exception{
+
+        mockMvc
+            .perform(mrb.get("/api/delete/backup/{tableId}/{date}", 6,"date1"))
+            .andDo(mrh.print())
+            .andExpect(mrm.status().isOk());
+    }
+
+    @Test
+    void deleteContentTest() throws Exception{
+
+        mockMvc
+            .perform(mrb.get("/api/delete/content/{tableId}/{contentId}", 6, "3126ce02-acf8-4b0b-b1c7-efe61e752f9f"))
+            .andDo(mrh.print())
+            .andExpect(mrm.redirectedUrl("/"));
+
+    }
+
+    @Test
+    void deleteFolderTest() throws Exception{
+
+        mockMvc
+            .perform(mrb.get("/api/delete/folder/{tableId}", 6))
+            .andDo(mrh.print())
+            .andExpect(mrm.status().isOk());
+    }
+
+
+}
